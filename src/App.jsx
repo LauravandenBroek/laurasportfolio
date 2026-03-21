3e7352a2278627382154ca9586a9fc27c28f56c2
LauravandenBroek <553467@student.fontys.nl>
2026-03-21 17:14:40 +0100

    Fix import case sensitivity for Navbar and Education components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './components/navigation'
+import Navbar from './components/Navigation'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Skills from './components/Skills'
 import AboutMe from './components/AboutMe'
 import Projects from './components/Projects'
-import Education from './components/education'
+import Education from './components/Education'
 
 function App() {
 
